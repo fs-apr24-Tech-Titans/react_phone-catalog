b63b69979c609cf57f3ebc23b5772041a8f16f2d
Oleksandr Pushyshyn <o.pushyshyn@gmail.com>
2024-07-30 17:46:36 +0300

    pink additional icon for FavAndCartBlock

--- src/components/FavAndCartBlock/FavAndCartBlock.tsx
+++ src/components/FavAndCartBlock/FavAndCartBlock.tsx
@@ -8,12 +8,18 @@ export const FavAndCartBlock: React.FC = () => {
       <div className="info-buttons__icon info-buttons__icon--favorites">
         <a className='info-buttons__link' href="#">
           <img src="/img/icons/favourites.svg" alt="" />
+          <span className="count">
+            <p className='countText'>1</p>
+          </span>
         </a>
       </div>
 
       <div className="info-buttons__icon info-buttons__icon--cart">
         <Link className='info-buttons__link' to="/cartPage">
           <img src="/img/icons/shopping-bag.svg" alt="" />
+          <span className="count">
+            <p className='countText'>1</p>
+          </span>
         </Link>
       </div>
     </div>
